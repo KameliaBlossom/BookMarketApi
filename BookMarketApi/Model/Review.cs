e4281b5349e2bf9da6d75c0f1be75ffeb20b7dc9
Инсаф Валиуллин <valiullininsaf2@gmail.com>
2025-07-12 16:13:43 +0300

    Добавлена логика регистрации/авторизации/аутенфикации

--- BookMarketApi/Model/Review.cs
+++ BookMarketApi/Model/Review.cs
@@ -2,5 +2,15 @@ namespace BookMarketApi.Model;
 
 public class Review
 {
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Content { get; set; }
+    public int Rating { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     
+    public Guid BookId { get; set; }
+    public virtual Book Book { get; set; }
+    
+    public Guid UserId { get; set; }
+    public virtual User User { get; set; }
+
 }
---
+++ BookMarketApi/Model/UserModel.cs
@@ -0,0 +1,9 @@
+﻿public class User
+{
+    public int Id { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string PasswordHash { get; set; }
+    public DateTime RegisterDate { get; set; }
+    public bool IsActive { get; set; }
+}
