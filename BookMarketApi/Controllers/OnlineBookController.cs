0c728b52fd8c697610d3dde4ecb5fb00b2a20edc
KameliaBlossom <anime.dabb@gmail.com>
2025-07-29 13:37:56 +0300

    Добавлены модели для: Детальной и Сокращенной книги, создания и апдейта книг.
    Реализован контролер и сервис для онлайн книг - GET SET PUT DELETE запросы
    
    Добавлен маппинг ДТО и сущности онлайн книги
    
    Добавлено отслеживание сервиса и репозитория для онлайн книг

---
+++ BookMarketApi/Controllers/OnlineBookController.cs
@@ -0,0 +1,87 @@
+﻿using BookMarketApi.DTOs;
+using BookMarketApi.Services.IServices;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookMarketApi.Controllers;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class OnlineBookController : ControllerBase
+{
+    private readonly IOnlineBookService _bookService;
+    
+    public OnlineBookController(IOnlineBookService bookService)
+    {
+        _bookService = bookService;
+    }
+
+    [HttpPost]
+    [Authorize]
+    [ProducesResponseType(typeof(OnlineBookDetailDTO), StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> CreateBook([FromBody] CreateOnlineBookDTO createDto)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        
+        var createdBook = await _bookService.CreateBookAsync(createDto);
+
+        return CreatedAtAction(nameof(createdBook), new { id = createdBook.Id }, createdBook);
+    }
+
+    [HttpGet("{id:guid}")]
+    [ProducesResponseType(typeof(OnlineBookDetailDTO), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> GetBookById(Guid id)
+    {
+        var book = await _bookService.GetBookByIdAsync(id);
+
+        return book == null ? NotFound() : Ok(book);
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(IEnumerable<OnlineBookShortDTO>), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetBooks()
+    {
+        var books = await _bookService.GetAllBooksAsync();
+        return Ok(books);
+    }
+
+    [HttpPut("{id:guid}")]
+    [ProducesResponseType(typeof(OnlineBookDetailDTO), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    public async Task<IActionResult> UpdateBook(Guid id, [FromBody] UpdateOnlineBookDTO updateDto)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest();
+        }
+        
+        var updatedBook = await _bookService.UpdateAsync(id, updateDto);
+        
+        return updatedBook == null ? NotFound() : Ok(updatedBook);
+    }
+
+    [HttpDelete("{id:guid}")]
+    [Authorize]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    public async Task<IActionResult> DeleteBook(Guid id)
+    {
+        var deletedBook = await _bookService.DeleteAsync(id);
+
+        if (deletedBook == null)
+        {
+            return NotFound();
+        }
+        
+        return Ok();
+    }
+}
---
+++ BookMarketApi/Services/OnlineBookService.cs
@@ -0,0 +1,69 @@
+﻿using AutoMapper;
+using BookMarketApi.DataAccess.Contracts;
+using BookMarketApi.DTOs;
+using BookMarketApi.Model;
+using BookMarketApi.Services.IServices;
+
+
+namespace BookMarketApi.Services;
+
+public class OnlineBookService : IOnlineBookService
+{
+    private readonly IOnlineBookRepository _bookRepository;
+    private readonly IMapper _mapper;
+
+    public OnlineBookService(IOnlineBookRepository bookRepository, IMapper mapper)
+    {
+        _bookRepository = bookRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<OnlineBookDetailDTO> CreateBookAsync(CreateOnlineBookDTO createDto)
+    {
+        var bookEntity = _mapper.Map<OnlineBook>(createDto);
+
+        var createdBook = await _bookRepository.AddAsync(bookEntity);
+
+        return _mapper.Map<OnlineBookDetailDTO>(createdBook);
+    }
+
+    public async Task<IEnumerable<OnlineBookShortDTO>> GetAllBooksAsync()
+    {
+        var books = await _bookRepository.GetAllAsync();
+        return _mapper.Map<IEnumerable<OnlineBookShortDTO>>(books);
+    }
+
+    public async Task<OnlineBookDetailDTO?> GetBookByIdAsync(Guid id)
+    {
+        var book = await _bookRepository.GetByIdAsync(id);
+        if (book == null)
+        {
+            return null;
+        }
+        return _mapper.Map<OnlineBookDetailDTO>(book);
+    }
+
+    public async Task<OnlineBookDetailDTO> UpdateAsync(Guid id, UpdateOnlineBookDTO updateDto)
+    {
+        var targetBook = await _bookRepository.GetByIdAsync(id);
+        if (targetBook == null)
+        {
+            return null;
+        }
+
+        _mapper.Map(updateDto, targetBook);
+        await _bookRepository.UpdateAsync(targetBook);
+        return _mapper.Map<OnlineBookDetailDTO>(targetBook);
+    }
+
+    public async Task<OnlineBookDetailDTO> DeleteAsync(Guid id)
+    {
+        var deleteBook = await _bookRepository.GetByIdAsync(id);
+        if (deleteBook == null)
+        {
+            return null;
+        }
+        await _bookRepository.DeleteByIdAsync(id);
+        return _mapper.Map<OnlineBookDetailDTO>(deleteBook);
+    }
+}
